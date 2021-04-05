f6ed55f2b91dc99f2162ad447362925e11ab09f4
Joel Masci <masci.joel@gmail.com>
2021-04-05 14:27:51 -0400

    fix(player-country): undelete config file

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// const url = 'http://localhost:3003/';
+const url = 'https://apitest.elma.online/';
+
+const s3SubFolder = 'test/';
+
+module.exports = {
+  url,
+  port: 3000,
+  s3SubFolder,
+  dlUrl: `${url}dl/`,
+  api: `${url}api/`,
+  up: `${url}u/`,
+  maps: 'AIzaSyDE8Prt4OybzNNxo1MzIn1XYNGxm9rI8Zk',
+  recaptcha: '6Le-n9QUAAAAAG-3bYyysXddxwD6I6iJeDBTHf2r',
+  maxUploadSize: 10485760,
+  s3Url: `https://eol.ams3.digitaloceanspaces.com/${s3SubFolder}`,
+};
