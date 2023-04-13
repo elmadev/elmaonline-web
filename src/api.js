b160adc82178cea70f05dcb1ed55dd633dc2f32c
metsavir <ismo.metsavirta@gmail.com>
2023-04-13 21:09:05 +0300

    feat(replays): add excluded tags filter to replay list

--- src/api.js
+++ src/api.js
@@ -110,7 +110,15 @@ export const ReplaysByLevelIndex = LevelIndex =>
   api.get(`replay/byLevelIndex/${LevelIndex}`);
 export const InsertReplay = data => api.post('replay', data);
 export const UpdateReplay = data => api.post('replay/update', data);
-export const Replays = ({ page, pageSize, tags, sortBy, order, levelPack }) => {
+export const Replays = ({
+  page,
+  pageSize,
+  tags,
+  sortBy,
+  order,
+  levelPack,
+  excludedTags,
+}) => {
   return api.get(`replay`, {
     page,
     pageSize,
@@ -118,6 +126,7 @@ export const Replays = ({ page, pageSize, tags, sortBy, order, levelPack }) => {
     sortBy,
     order,
     levelPack,
+    excludedTags,
   });
 };
 export const AllMyReplays = ({ page, pageSize, tags, sortBy, order }) => {
