76773be10bb0c6e95891781a1d846d62c302fe6b
Sune <sunehs@gmail.com>
2024-05-18 21:10:26 +0200

    feat(other): add hotjar

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StoreProvider, createStore } from 'easy-peasy';
 import { HelmetProvider } from 'react-helmet-async';
 import Router from './router';
 import model from './easypeasy';
 import { queryClient } from './react-query';
 import { QueryClientProvider } from 'react-query';
+import Hotjar from '@hotjar/browser';
+import config from 'config';
 
 const easyPeasyStore = createStore(model, {
   version: 2,
 });
 
 function App() {
+  useEffect(() => {
+    if (config.hotJarId) {
+      Hotjar.init(config.hotJarId, 6);
+    }
+  }, []);
   return (
     <StoreProvider store={easyPeasyStore}>
       <QueryClientProvider client={queryClient}>
