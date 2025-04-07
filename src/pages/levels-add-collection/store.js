fcc6173433922d93473af52e8dde4c511b8df51c
sunehs <sunehs@gmail.com>
2025-04-07 22:04:46 +0200

    fix(levelpacks): check for existing levelpack name

--- src/pages/levels-add-collection/store.js
+++ src/pages/levels-add-collection/store.js
@@ -6,8 +6,16 @@ export default {
   setAddSuccess: action((state, payload) => {
     state.addSuccess = payload;
   }),
+  error: '',
+  setError: action((state, payload) => {
+    state.error = payload;
+  }),
   addCollection: thunk(async (actions, payload) => {
     const add = await AddCollection(payload);
+    if (add.data.error) {
+      actions.setError(add.data.error);
+      return;
+    }
     if (add.ok) {
       actions.setAddSuccess(payload.CollectionName);
     }
