f6a034fd4e8a1674139bf301d49d7e70ae5e647b
sunehs <sunehs@gmail.com>
2025-05-18 21:10:27 +0200

    refactor(battles): played battles - react query

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import config from 'config';
 import { authToken } from 'utils/nick';
 import assert from 'assert';
 import { isObjectLike, isArray, mapValues, meanBy } from 'lodash';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
 
 let baseURL = config.api;
 const api = create({
@@ -85,6 +85,8 @@ export const useQueryAlt = (
     ...queryOpts,
   });
 };
+
+export { keepPreviousData };
 
 // replays
 export const ReplayComment = replayIndex =>
@@ -465,7 +467,8 @@ export const SearchChat = data =>
 export const LGR = LGRName => api.get(`lgr/info/${LGRName}`);
 export const LGRs = () => api.get(`lgr/info`);
 export const NewLGR = formData => apiUpload.post('api/lgr/add', formData);
-export const EditLGR = (LGRName, formData) => apiUpload.post(`api/lgr/info/${LGRName}`, formData);
+export const EditLGR = (LGRName, formData) =>
+  apiUpload.post(`api/lgr/info/${LGRName}`, formData);
 export const DeleteLGR = LGRName => api.delete(`lgr/del/${LGRName}`);
 export const LGRComments = LGRIndex => api.get(`lgr_comment/get/${LGRIndex}`);
 export const NewLGRComment = data => api.post(`lgr_comment/add`, data);
